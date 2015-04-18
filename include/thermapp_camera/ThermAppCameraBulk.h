c0b9065ab0bea7febf550afe4961416d21ca0b81
Christian Holl <CyborgX1@gmail.com>
2015-04-19 00:18:35 +0200

    add class for usb bulk transfer

---
+++ include/thermapp_camera/ThermAppCameraBulk.h
@@ -0,0 +1,21 @@
+/*
+ * ThermAppCameraBulk.h
+ *
+ *  Created on: Apr 18, 2015
+ *      Author: cyborg-x1
+ */
+
+#ifndef THERMAPP_CAMERA_SRC_THERMAPPCAMERABULK_H_
+#define THERMAPP_CAMERA_SRC_THERMAPPCAMERABULK_H_
+
+namespace ThermApp {
+
+class ThermAppCameraBulk {
+public:
+	ThermAppCameraBulk();
+	virtual ~ThermAppCameraBulk();
+};
+
+} /* namespace ThermApp */
+
+#endif /* THERMAPP_CAMERA_SRC_THERMAPPCAMERABULK_H_ */
---
+++ src/ThermAppCameraBulk.cpp
@@ -0,0 +1,21 @@
+/*
+ * ThermAppCameraBulk.cpp
+ *
+ *  Created on: Apr 18, 2015
+ *      Author: cyborg-x1
+ */
+
+#include "thermapp_camera/ThermAppCameraBulk.h"
+
+namespace ThermApp {
+
+ThermAppCameraBulk::ThermAppCameraBulk() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ThermAppCameraBulk::~ThermAppCameraBulk() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace ThermApp */
