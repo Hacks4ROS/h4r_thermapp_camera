9769d8faf22ba42ab11b2949c845997eb8dbe663
Christian Holl <CyborgX1@gmail.com>
2015-04-24 00:23:05 +0200

    create include for catkin for not breaking up with origin of the thermapp code

---
+++ include/thermapp_camera/libthermapp.h
@@ -0,0 +1,13 @@
+/*
+ * libthermapp.h
+ *
+ *  Just a wrapper for catkin for not breaking the link
+ *  to original repository
+ */
+
+#ifndef LIBTHERMAPP_H_
+#define LIBTHERMAPP_H_
+
+#include "../src/ThermAppCam/thermapp/thermapp.h"
+
+#endif /* LIBTHERMAPP_H_ */
